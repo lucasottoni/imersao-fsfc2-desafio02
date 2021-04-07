1ad4d31e8e9553e86e390573c93c66b41fa3da65
Lucas Daniel Ottoni <lucas.ottoni@zup.com.br>
2021-04-07 19:19:43 -0300

    adding more routes to mongo init

--- nest-api/.docker/mongo/init.js
+++ nest-api/.docker/mongo/init.js
@@ -17,4 +17,16 @@ db.routes.insertMany([
     startPosition: { lat: -15.82331, lng: -47.92588 },
     endPosition: { lat: -15.82758, lng: -47.92532 },
   },
+  {
+    _id: '4',
+    title: 'Quarto',
+    startPosition: { lat: -22.82331, lng: -47.92588 },
+    endPosition: { lat: -15.82758, lng: -44.92532 },
+  },
+  {
+    _id: '5',
+    title: 'Quinto',
+    startPosition: { lat: -15.82331, lng: -22.92588 },
+    endPosition: { lat: -33.82758, lng: -99.92532 },
+  },
 ]);
